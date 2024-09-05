f42816ece181b8988e2da8e5bbcdd97f148c0a7b
Olsney <104034829+Olsney@users.noreply.github.com>
2024-09-06 01:50:25 +0300

    First init

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Timers;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private float _velocity = 10f;
+
+    private Vector3 _direction;
+    private Vector3 _rotation;
+
+    public void Update()
+    {
+        transform.position += _direction * Time.deltaTime;
+        transform.rotation = Quaternion.Euler(_rotation);
+    }
+
+    public void Init(Vector3 rotation, Vector3 direction)
+    {
+        _rotation = rotation;
+        _direction = direction;
+    }
+}
